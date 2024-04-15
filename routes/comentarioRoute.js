7d44db16f559dd1338c9bda9ee83d5f9607ac20b
bautipeleritti <bpeleritti@udesa.edu.ar>
2024-04-15 16:03:29 -0300

    .

---
+++ routes/comentarioRoute.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const comentarioController = require('../controllers/comentarioController')
+
+
+
+
+
+module.exports = router
---
+++ routes/productoRoute.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const router = express.Router();
+const productoController = require('../controllers/productoController')
+module.exports = router
