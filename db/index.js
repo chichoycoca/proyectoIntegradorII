3a36643bc82522619cb72b3871f733c7a9d97c4d
mativb <mvalentinobarzi@udesa.edu.ar>
2024-04-15 18:40:40 -0300

    db

--- db/index.js
+++ db/index.js
@@ -1,12 +1,54 @@
 const db = {
     usuario:{
-        email:'simonc@gmail.com',
+        1: {
+        id: '1',
+        email:'simocahn@gmail.com',
         usuario: 'chichocahn',
-        contrasena: 'simon123',
-        fechaNacimiento: '02/05/2005',
+        contrasena: 'simon',
+        fechaNacimiento: '2020-05-02',
         dni: '46649856',
+        fotoPerfil: '/images/fotografo-retrato-corporativo-linkedin-7.jpg'
+        },
+        2: {
+        id: '2',
+        email:'matiasvb@gmail.com',
+        usuario: 'mativb',
+        contrasena: 'matias',
+        fechaNacimiento: '02/05/2005',
+        dni: '46649857',
+        fotoPerfil: '/images/fotos-para-perfil-de-linkedin-hombre.webp'
+        },
+        3: {
+        id: '3',
+        email:'bautip@gmail.com',
+        usuario: 'bautipele',
+        contrasena: 'bauti',
+        fechaNacimiento: '02/05/2005',
+        dni: '46649858',
+        fotoPerfil: '/images/'
+        },
+        4: {
+        id: '4',
+        email:'tomyc@gmail.com',
+        usuario: 'tomycas',
+        contrasena: 'tomi',
+        fechaNacimiento: '02/05/2005',
+        dni: '46649859',
         fotoPerfil: 'foto'
+        },
+        5: {
+        id: '5',
+        email:'juanf@gmail.com',
+        usuario: 'juanf',
+        contrasena: 'juan',
+        fechaNacimiento: '02/05/2005',
+        dni: '46649860',
+        fotoPerfil: 'foto'
+        },
     },
+
+
+
     producto:[{        
             id: '1',
             imagen: 'foto',
