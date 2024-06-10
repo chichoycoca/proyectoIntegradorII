57cb7c5036400329213e09008cfdb8dbe033f75f
bautipeleritti <bpeleritti@udesa.edu.ar>
2024-06-10 15:21:43 -0300

    modeloproductos

---
+++ database/models/modeloProducto.js
@@ -0,0 +1,30 @@
+const { type } = require("express/lib/response")
+
+module.exports = function(sequelize, dataTypes){
+    let alias = "Producto"
+    let cols = {
+        id:{
+            autoincrement: true,
+            primaryKey: true,
+            type: dataTypes.INTEGER,
+        },
+        imagen_producto:{
+            type: dataTypes.STRING
+        },
+        producto:{
+            type: dataTypes.STRING
+        },
+        descripcion:{
+            type: dataTypes.STRING
+        },
+        cliente_id:{
+            type: dataTypes.INTEGER
+        }
+         
+    }
+    let config = {
+        tableName: "productos",
+        timestamps: true,
+        underscored: true
+    }
+}
